4e1b66d16382b711f07b65fbe7611cb5b00eff08
de-lolec <m_egrishin@webjox.ru>
2023-12-26 01:45:50 +0300

    feat: refactor register page and added axios setting

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -5,46 +5,107 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
-        <div>
-          <label for="fullname" class="block text-sm font-medium leading-6 text-gray-900">ФИО</label>
-          <div class="mt-2">
-            <input id="fullname" name="fullname" type="text" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-        <div>
-          <label for="fullname" class="block text-sm font-medium leading-6 text-gray-900">Страна рождения</label>
-          <div class="mt-2">
-            <select-country class="mt-0"/>
-          </div>
-        </div>
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-          <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Пароль</label>
-          </div>
-          <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Повторите пароль</label>
-          </div>
-          <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Зарегистрироваться</button>
-        </div>
-      </form>
+        <form class="mt-8 space-y-6" @submit="register">
+    
+            <input type="hidden" name="remember" value="true" />
+            <div class="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label for="fullname" class="sr-only">Email address</label>
+                <input
+                  id="fullname"
+                  name="name"
+                  type="text"
+                  autocomplete="name"
+                  required=""
+                  v-model="user.name"
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Full name"
+                />
+              </div>
+              <div>
+                <label for="email-address" class="sr-only">Email address</label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required=""
+                  v-model="user.email"
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  :class="{ 'border-red-500': errors.email, 'z-10': errors.email }"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label for="password" class="sr-only">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required=""
+                  v-model="user.password"
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  :class="{ 'border-red-500': errors.password, 'z-10': errors.password }"
+                />
+              </div>
+              <div>
+                <label for="password_confirmation" class="sr-only">Password</label>
+                <input
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  type="password"
+                  autocomplete="current-password"
+                  required=""
+                  v-model="user.password_confirmation"
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+        
+            <div>
+              <button
+                type="submit"
+                :disabled="loading"
+                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                :class="{
+                  'cursor-not-allowed': loading,
+                  'hover:bg-indigo-500': loading,
+                }"
+              >
+                <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                <svg
+                  v-if="loading"
+                  class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Sign up
+              </button>
+            </div>
+          </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
         <router-link :to="{name: 'Login'}" href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Войти</router-link>
@@ -53,15 +114,38 @@
   </div>
 </template>
 
-<script>
-import SelectCountry from '../components/SelectCountry.vue';
+<script setup>
+import { ref } from "vue";
+import store from "../store";
+import { useRouter } from "vue-router";
 
-  export default {
-    
-    components: { SelectCountry }
-  }
+const router = useRouter();
+const user = {
+  name: "",
+  email: "",
+  password: "",
+};
+const loading = ref(false);
+const errors = ref({});
 
-  
+function register(ev) {
+  ev.preventDefault();
+  loading.value = true;
+  store
+    .dispatch("register", user)
+    .then(() => {
+      loading.value = false;
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch((error) => {
+      loading.value = false;
+      if (error.response.status === 422) {
+        errors.value = error.response.data.errors;
+      }
+    });
+}
 </script>
 
 <style scoped>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,5 @@
 import {createStore} from "vuex";
+import axiosClient from "../axios";
 
 const store = createStore({
   state: {
@@ -11,7 +12,31 @@ const store = createStore({
     }
   },
   getters: {},
-  actions: {},
+  actions: {
+    register({commit}, user) {
+        return axiosClient.post('/register', user)
+          .then(({data}) => {
+            commit('setUser', data.user);
+            commit('setToken', data.token)
+            return data;
+          })
+      },
+      login({commit}, user) {
+        return axiosClient.post('/login', user)
+          .then(({data}) => {
+            commit('setUser', data.user);
+            commit('setToken', data.token)
+            return data;
+          })
+      },
+      logout({commit}) {
+        return axiosClient.post('/logout')
+          .then(response => {
+            commit('logout')
+            return response;
+          })
+      },
+  },
   mutations: {},
   modules: {}
 })
