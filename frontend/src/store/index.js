f65a551135d4d5f5d5e76f8cd8acab342cbba9b2
de-lolec <m_egrishin@webjox.ru>
2023-12-26 02:21:26 +0300

    dev: added env

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -37,7 +37,21 @@ const store = createStore({
           })
       },
   },
-  mutations: {},
+  mutations: {
+    logout: (state) => {
+        state.user.token = null;
+        state.user.data = {};
+        sessionStorage.removeItem("TOKEN");
+      },
+  
+      setUser: (state, user) => {
+        state.user.data = user;
+      },
+      setToken: (state, token) => {
+        state.user.token = token;
+        sessionStorage.setItem('TOKEN', token);
+      },
+  },
   modules: {}
 })
 
