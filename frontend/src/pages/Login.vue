3676b4def791b2b07b7367ce53e97120777f8b31
de-lolec <m_egrishin@webjox.ru>
2023-12-26 04:20:09 +0300

    dev: refactoring auth vue templates

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -16,7 +16,7 @@
               autocomplete="email"
               required=""
               v-model="user.email"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Email address"
             />
           </div>
@@ -37,7 +37,7 @@
               autocomplete="current-password"
               required=""
               v-model="user.password"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Password"
             />
           </div>
