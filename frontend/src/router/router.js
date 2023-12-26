47911d5b4efbe142c42a509a9d26a8cbbb36ce3f
de-lolec <m_egrishin@webjox.ru>
2023-12-26 05:06:08 +0300

    feat: added template catalog

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
+import Catalog from "../pages/Catalog.vue";
 import Dashboard from "../pages/Dashboard.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import store from "../store";
@@ -24,6 +25,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/catalog',
+    name: 'Catalog',
+    component: Catalog
   },
 ];
 
